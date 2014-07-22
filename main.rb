71488b518861d83993552f02e373547fbbabf108
Maimer <nesnl23@yahoo.com>
2014-07-21 20:22:35 -0400

    Initial commit

---
+++ main.rb
@@ -0,0 +1,21 @@
+require 'gosu'
+
+class Main < Gosu::Window
+  SCREEN_WIDTH = 1024
+  SCREEN_HEIGHT = 1024
+
+  def initialize
+    super(SCREEN_WIDTH, SCREEN_HEIGHT, false)
+    self.caption = "Squares"
+
+  end
+
+  def update
+
+  end
+
+  def draw
+
+  end
+
+end
