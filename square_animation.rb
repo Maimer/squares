59b91919b2d7befaedb34b1884fea825dbef31d1
Maimer <nesnl23@yahoo.com>
2014-08-22 22:38:18 -0400

    Added scoring text in center of squares

--- square_animation.rb
+++ square_animation.rb
@@ -3,15 +3,16 @@ class SquareAnimation
   def initialize(window, p1, p2, p3, p4, color, board)
     @window = window
     @board = board
+    @message = "+#{([p1[0], p2[0], p3[0], p4[0]].max - [p1[0], p2[0], p3[0], p4[0]].min + 1) ** 2}"
+    @score_font = Gosu::Font.new(window, "Tahoma", SCREEN_HEIGHT / 16)
     @p1 = [p1[0] * @board.orange.width + (@board.orange.width / 2) + @board.origin,
-           p1[1] * @board.orange.height + (@board.orange.width / 2) + @board.origin]
+           p1[1] * @board.orange.height + (@board.orange.height / 2) + @board.origin]
     @p2 = [p2[0] * @board.orange.width + (@board.orange.width / 2) + @board.origin,
-           p2[1] * @board.orange.height + (@board.orange.width / 2) + @board.origin]
+           p2[1] * @board.orange.height + (@board.orange.height / 2) + @board.origin]
     @p3 = [p3[0] * @board.orange.width + (@board.orange.width / 2) + @board.origin,
-           p3[1] * @board.orange.height + (@board.orange.width / 2) + @board.origin]
+           p3[1] * @board.orange.height + (@board.orange.height / 2) + @board.origin]
     @p4 = [p4[0] * @board.orange.width + (@board.orange.width / 2) + @board.origin,
-           p4[1] * @board.orange.height + (@board.orange.width / 2) + @board.origin]
-    @c = color
+           p4[1] * @board.orange.height + (@board.orange.height / 2) + @board.origin]
     color == "O" ? @color = Gosu::Color.argb(100, 255, 91, 0) : @color = Gosu::Color.argb(100, 0, 43, 255)
     @time = Gosu::milliseconds
   end
@@ -26,11 +27,17 @@ class SquareAnimation
 
   def draw
     if @color.alpha > 0
-      #draw scoring text
-    end
+      square_x = ([@p1[0], @p2[0], @p3[0], @p4[0]].max + [@p1[0], @p2[0], @p3[0], @p4[0]].min) / 2
+      square_y = ([@p1[1], @p2[1], @p3[1], @p4[1]].max + [@p1[1], @p2[1], @p3[1], @p4[1]].min) / 2
+      Drawing::draw_text(square_x - @score_font.text_width(@message) / 2,
+                         square_y - @score_font.height / 2 - 5,
+                         @message,
+                         @score_font,
+                         Gosu::Color::WHITE)
     @window.draw_quad(@p1[0], @p1[1], @color,
                       @p2[0], @p2[1], @color,
                       @p3[0], @p3[1], @color,
                       @p4[0], @p4[1], @color, 1)
+    end
   end
 end
